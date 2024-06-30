a1c9f2337ee0e757e23e76e1bf26b33bada64609
Danilo Faccio <facciodrums@gmail.com>
2024-06-30 17:49:25 -0300

    criando registry

---
+++ src/test/java/com/ecommerce_registry/EcommerceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
